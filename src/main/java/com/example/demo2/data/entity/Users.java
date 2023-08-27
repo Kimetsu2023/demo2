5d96986d0a4952e0bc9fec6ee899d5be68db53c1
Masayuki Emoto <137867861+kimetsu2023@users.noreply.github.com>
2023-08-27 21:50:02 +0900

    認可機能の実装完了

---
+++ src/main/java/com/example/demo2/data/entity/Users.java
@@ -0,0 +1,18 @@
+package com.example.demo2.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Entity
+@ToString(exclude = "password") // ©“®¶¬‚³‚ê‚étoString‚Épassword‚ğo—Í‚µ‚È‚¢
+public class Users {
+	@Id
+	public String userName;
+	public String password;
+	public String name;
+	public String roleName;
+}
